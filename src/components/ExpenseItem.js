833b56782a9c812b78c1a511f77e3120302c6fb9
Matej Mahút <matthew3993@gmail.com>
2022-10-31 23:11:30 +0100

    Added styles

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amout</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // makes feature in library 'react-dom' available in this file 'index.js'
 
 import './index.css';
 import App from './App';
