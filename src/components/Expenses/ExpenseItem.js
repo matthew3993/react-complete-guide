1ad7c6a7e6c8199ebcf079e5978e3bd2cc1f6d6f
Matej Mahút <mahut@inprop.sk>
2022-11-21 10:24:17 +0100

    Finished lecture 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react'; // in {} braces - named import
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    // 'useState' is one of React's hooks. Hooks start with word 'use'
+    // convection: [{nameOfVariable}, set{NameOfVariable}]
+    // setTitle is so called 'state updating function'
+    const [title, setTitle] = useState(props.title); // registering the hook
 
-    let title = props.title;
+    // let title = props.title;
 
     const clickHandler = () => {
-        title = 'Updated!';
+        // title = 'Updated!';
+        setTitle('Updated!'); // calling 'state updating function' will execute 'parent' component function
+        
+        // calling 'state updating function' doesn't change the value right away, but instead it shedules the state update
+        // that is the eason why old (not updated) values are logged
         console.log(title);
     };
 
