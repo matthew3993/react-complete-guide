ac0121e95475b596f618d6ad7641405545285fe8
Matej Mahút <mahut@inprop.sk>
2022-11-23 11:40:34 +0100

    Finished lecture 57

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,6 +63,11 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+
+        // clear the form
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -70,7 +75,11 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
@@ -78,6 +87,7 @@ const ExpenseForm = () => {
                         type='number'
                         min='0.01'
                         step='0.01'
+                        value={enteredAmount}
                         onChange={amountChangeHandler}
                     />
                 </div>
@@ -87,6 +97,7 @@ const ExpenseForm = () => {
                         type='date'
                         min='2019-01-01'
                         max='2022-12-31'
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
