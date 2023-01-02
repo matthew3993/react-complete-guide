70c30abc0f733ec52e854d660f418b2883aa6370
Matej Mahút <mahut@inprop.sk>
2023-01-02 14:27:45 +0100

    Finished lecture 63

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // in {} braces - named import
+import React from 'react'; // in {} braces - named import
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -10,28 +10,27 @@ const ExpenseItem = (props) => {
     // setTitle is so called 'state updating function'
     // useState registers 'state' (~ some value as a state) for the component INSTANCE in which it is being called
     // each component instance has it's own state - it on 'per component instance' basis
-    const [title, setTitle] = useState(props.title); // registering the hook
-    console.log('ExpenseItem evaluated by React');
+    // const [title, setTitle] = useState(props.title); // registering the hook
+    // console.log('ExpenseItem evaluated by React');
 
     // let title = props.title;
 
-    const clickHandler = () => {
-        // title = 'Updated!';
-        setTitle('Updated!'); // calling 'state updating function' will execute 'parent' component function
+    // const clickHandler = () => {
+        // title = 'Updated!'; // this will not update the value in UI !
+        // setTitle('Updated!'); // calling 'state updating function' will execute 'parent' component function
         
         // calling 'state updating function' doesn't change the value right away, but instead it shedules the state update
         // that is the eason why old (not updated) values are logged
-        console.log(title);
-    };
+        // console.log(title);
+    // };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
