2f91729a3a327ca1fb57cba01bcaf5d9c42368a1
Matej Mahút <mahut@inprop.sk>
2023-01-26 14:51:05 +0100

    understood assigment 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,13 +6,17 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    // console.log('Expenses: ---START---');
     const [selectedYear, setSelectedYear] = useState('2020');
 
     const yearChangeHandler = (selectedYear) => {
-        console.log('Expenses: setting selectedYear = ' + selectedYear);
+        // console.log('yearChangeHandler: setting selectedYear = ' + selectedYear);
         setSelectedYear(selectedYear);
     };
 
+    // HOW IS THIS WORKING?!?!? => It is working, because if State is updated, function (component) to which it belong is called - RE-evaulated (enable logs and check them in console)\
+    // see notes in 'ExpenseItem' - calling 'state updating function' will execute 'parent' component function
+    // console.log('Expenses: filtering expenses..., selectedYear: ', selectedYear);
     const filteredExpenses = props.items
         .filter(expense => expense.date.getFullYear().toString() === selectedYear);
 
